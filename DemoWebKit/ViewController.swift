c178383e53e41d8560e88e6e69ba6373b8326d0b
quanzen8labs <quan@zen8labs.com>
2022-11-30 13:15:08 +0900

    first commit

--- DemoWebKit/ViewController.swift
+++ DemoWebKit/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.navigationDelegate = self
+        webView.load(URLRequest(url: URL(string: "https://infoheap.com/html-form-create-request-and-submit/")!))
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction) async -> WKNavigationActionPolicy {
+        if let httpBody = navigationAction.request.httpBody {
+            if let params = String(data: httpBody, encoding: .utf8) {
+                let uiAlertViewController = UIAlertController(
+                    title: navigationAction.request.url?.absoluteString ?? "", message: "Body params: \n \(params)", preferredStyle: .alert)
+                uiAlertViewController.addAction(UIAlertAction(title: "OK", style: .cancel))
+                self.present(uiAlertViewController, animated: true)
+            }
+        }
+        return .allow
+    }
 }
 
